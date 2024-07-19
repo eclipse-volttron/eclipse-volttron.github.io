868309a1d04c45c2a79351e39e519cec0fe1b7b3
Claire <claire.monson@pnnl.gov>
2024-07-19 11:49:08 -0700

    dynamic adjustments

--- public/js/main.js
+++ public/js/main.js
@@ -103,7 +103,7 @@
           }
           slides[slideIndex-1].style.display = "block";  
           dots[slideIndex-1].className += " active";
-          setTimeout(showSlides, 8000); // Change image every 2 seconds
+          setTimeout(showSlides, 20000); // Change image every 10 seconds
         }
 
 
